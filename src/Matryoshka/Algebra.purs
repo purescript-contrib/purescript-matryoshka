25071b866e9f0ce33c00e48568b9d495f65a0e9b
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-12-11 12:30:05 -0800

    Add kind annotations

--- src/Matryoshka/Algebra.purs
+++ src/Matryoshka/Algebra.purs
@@ -16,12 +16,16 @@ limitations under the License.
 
 module Matryoshka.Algebra where
 
+type GAlgebra :: (Type -> Type) -> (Type -> Type) -> Type -> Type
 type GAlgebra w f a = f (w a) → a
 
+type GAlgebraM :: (Type -> Type) -> (Type -> Type) -> (Type -> Type) -> Type -> Type
 type GAlgebraM w m f a = f (w a) → m a
 
 type Algebra f a = f a → a
 
+type AlgebraM :: (Type -> Type) -> (Type -> Type) -> Type -> Type
 type AlgebraM m f a = f a → m a
 
+type ElgotAlgebra :: (Type -> Type) -> (Type -> Type) -> Type -> Type
 type ElgotAlgebra w f a = w (f a) → a
